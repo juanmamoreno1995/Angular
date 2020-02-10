bd0407133b6ed0a3b5f5d4398dc53e29597cc5bb
juanmamoreno1995 <juanmamorenogarrido@gmail.com>
2020-02-10 19:33:33 +0100

    ngmodel error

--- petclinicT4/src/app/components/form-owner/form-owner.component.ts
+++ petclinicT4/src/app/components/form-owner/form-owner.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OwnerService } from 'src/app/servicios/owner.service';
 import { Owners } from 'src/app/models/owners';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-owner',
@@ -17,9 +18,9 @@ export class FormOwnerComponent implements OnInit {
   // private ciudad : string;
   // private tel : string;
 
-  constructor(private servicioOw : OwnerService) { 
+  constructor(private servicioOw : OwnerService,private ruta: Router) { 
     this.owner = <Owners>{
-      firstName : "",
+      firstName : "pep",
       lastName: "",
       address:"",
       city:"",
@@ -28,7 +29,15 @@ export class FormOwnerComponent implements OnInit {
   }
 
   anhadir(){
-    console.log(this.owner.firstName)
+    var owner = {
+      firstName : this.owner.firstName,
+      lastName: this.owner.lastName,
+      address:this.owner.address,
+      city:this.owner.city,
+      telephone:this.owner.telephone
+    };
+    console.log(owner.firstName);
+    console.log("dentro añadir")
   }
 
   ngOnInit() {
