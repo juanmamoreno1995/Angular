7f881987504954ea3322c5c78bfb798c3f73b1a4
juanmamoreno1995 <juanmamorenogarrido@gmail.com>
2020-02-19 17:53:00 +0100

    Hacer añadir

--- petclinicT4/src/app/form-visit/form-visit.component.ts
+++ petclinicT4/src/app/form-visit/form-visit.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PetService } from '../servicios/pet.service';
 import { Pet } from '../models/pet';
+import { Visits } from '../models/visits';
 
 @Component({
   selector: 'app-form-visit',
@@ -11,18 +12,30 @@ import { Pet } from '../models/pet';
 })
 export class FormVisitComponent implements OnInit {
   private pet: Pet;
-  private idPet:number;
-  private vacio : boolean;
-  
+  private idPet: number;
+  private vacio: boolean;
+  private visita: Visits;
+
   constructor(private http: HttpClient, private ruta: Router, private parametro: ActivatedRoute, private servPet: PetService) {
     this.pet = <Pet>{};
     this.vacio = false;
 
+    this.visita = <Visits>{};
+
   }
 
+  anhadir() {
+    console.log(this.visita);
+  }
+
+
+
   ngOnInit() {
+
     this.idPet = this.parametro.snapshot.params["idPet"];
-    this.servPet.unPet(this.idPet).subscribe(datos=>{
+    console.log(this.idPet)
+    this.servPet.unPet(this.idPet).subscribe(datos => {
+
       this.pet = datos;
       console.log(this.pet);
       this.vacio = true;
